7703ca2f667ac59067b51970075e63f02b6c4dea
RickCraco <riccardocracolici43@gmail.com>
2023-11-08 14:13:16 +0100

    Creato scaffholding usando vite

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        titolo: 'La mia prima app con Vite'
+      }
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+
+</style>
