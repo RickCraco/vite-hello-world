773d986439eb89d9bc552009f8180c2541624781
RickCraco <riccardocracolici43@gmail.com>
2023-11-08 15:35:23 +0100

    aggiunto componente AppHero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img :src="immagine" alt="">
+        <h2>{{titolo}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppHero",
+        data(){
+            return{
+                immagine: 'https://thesoundcheck.it/wp-content/uploads/2023/06/gear3-1024x576.jpg',
+                titolo: 'Jumbo'
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
